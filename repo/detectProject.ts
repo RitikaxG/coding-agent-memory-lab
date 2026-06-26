daeb33d218c7836653523beaa88b70a75a1ff02a
Ritikag33 <ritikag5533@gmail.com>
2026-06-26 15:21:10 +0530

    initialising repo map files

---
+++ repo/detectProject.ts
@@ -0,0 +1,6 @@
+/*
+Detects :
+1. package manager - pnpm, bun , npm, yarn
+2. framework - nextjs, express, react
+3. scripts - test, lint, typecheck, dev , build
+*/
---
+++ repo/scanRepo.ts
@@ -0,0 +1,6 @@
+/*
+- recursively scan files
+- ignore node_modules, .git, dist, build, .next, coverage
+- return file paths
+- detect important files
+*/
